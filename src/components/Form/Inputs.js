4c53a7d47d4063ec9acf602c6a0538233e130f99
walissonsou <walissonsouza96@hotmail.com>
2022-05-06 00:48:21 -0300

    delete

--- src/components/Form/Inputs.js
+++ src/components/Form/Inputs.js
@@ -6,29 +6,32 @@ import './style.css';
 import Button from '@mui/material/Button';
 import { db } from '../../firebase'
 import { uid } from "uid"
-import { set, ref, onValue  } from 'firebase/database'
+import { set, ref, onValue , remove } from 'firebase/database'
 import { useState , useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import Fingerprint from '@mui/icons-material/Fingerprint'
 
 import DeleteIcon from '@mui/icons-material/Delete';
 
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
 
  
 export function Inputs() {   
 
   const [ todo , setTodo ] = useState("")
   const [ items , setItems] = useState([])
+
   const lidandoComMudançadoTodo = (e) => {
     setTodo(e.target.value);
   };
     //read
     useEffect (() => {
       onValue(ref(db), snapshot => {
+        setItems([])
         const data = snapshot.val()
         if(data !== null) {
           Object.values(data).map(todo => {
+            
             setItems(oldItems => [...oldItems, todo])
           })
         }
@@ -45,6 +48,10 @@ export function Inputs() {
    })
    setTodo('')
   }
+  const lidandoDeletando = (todo) => {
+    remove(ref(db, `/${todo.uuid}`));
+
+  };
 
         return (
     <Box
@@ -62,11 +69,12 @@ export function Inputs() {
          <div className="lista">
          <ul>
             <li>
-            { todo.todo }
-            <IconButton aria-label="delete" size="large">
+            {todo.todo}
+            <IconButton aria-label="delete" size="large"
+             onClick={() => lidandoDeletando(todo)}>
               <DeleteIcon fontSize="inherit" />
             </IconButton>
-            <IconButton aria-label="fingerprint" color="success">
+            <IconButton aria-label="fingerprint" color="success" >
               <Fingerprint />
             </IconButton>            
             </li>           
